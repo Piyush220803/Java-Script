7eecf4c156634c5f6811da971ae6c05ab72ed2a0
Piyush220803 <158078484+Piyush220803@users.noreply.github.com>
2024-08-02 22:17:42 +0530

    Add files via upload

---
+++ 01_basics/01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+// let score = "45abc"
+// let score = null // data type = object and O/P-> 0
+// let score = undefined O/P-> NaN
+// score = true OP = 1 data type = boolean
+
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valInNumber = Number(score)
+// console.log(typeof valInNumber) // as we know logically 45abc can not be converted into number
+// console.log(valInNumber)  // O/P-> NaN
+
+// "45" => 45
+// "45abc" => Nan
+// true = 1; false = 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn) 
+
+
+// 1 = true; false = 0
+// "" = false
+// "piyush" = true
+
+// ********************* OPERATIONS ********************
+
+let value = 4
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2-2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = "piyush"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2);  // 122
+// console.log(1 + 2 + "2"); // 32
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
