9070696f82499545099a30b7827954b7e2186522
Piyush220803 <158078484+Piyush220803@users.noreply.github.com>
2024-08-04 13:46:37 +0530

    Add files via upload

---
+++ 01_basics/01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log( 2 > 1);
+// console.log( 2 == 1);
+// console.log( 2 < 1);
+// console.log( 2 >= 1);
+// console.log( 2 != 1);
+
+// console.log( "2" > 1);  Here 2 written in the form of string but js convert it into number thats why the result is true
+
+// console.log( null > 0);   false          // Avoid
+// console.log( null < 0);   false              using
+// console.log( null == 0); false               this 
+// console.log( null >= 0); true                type
+// console.log( undefined > 0);  false          of
+// console.log( undefined < 0);    false        code    
+// console.log( undefined == 0);   false        to be write
+
+// console.log( "2" === 2); When we use three operator like here "===" it not change the datatype to number so the Output is false
+
+// using 2 operator will change the datatype
