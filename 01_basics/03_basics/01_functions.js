3c041747a725df520bb07f31028d4ed590d4c3a2
Piyush220803 <158078484+Piyush220803@users.noreply.github.com>
2024-08-07 19:32:16 +0530

    01_functions.js

--- 01_basics/03_basics/01_functions.js
+++ 01_basics/03_basics/01_functions.js
@@ -44,3 +44,34 @@ function loginUserMessage(username = "Tony") {  // tony = default value
 // console.log(loginUserMessage());   // undefined as there is no arguments passed
 console.log(loginUserMessage()); // for not getting the undefined result we can set a default value to the function in case there is no argument passed and if we pass the argument then the passed argument will over ride the default
 
+//  function calculateCartPrice(...num1){ //  this "..." is use here because here we pass only one parameter but if we pass more arguments when calling the function then it will be stord in the from of array 
+//     return num1
+//  }
+//  console.log(calculateCartPrice(200, 400, 600));
+ 
+//  const user = {
+//     username: "Piyush",
+//     price: 199
+//  }
+
+//  function handleObject(anyobject) {
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+//  }
+
+//  handleObject(user)
+
+// also we can pass the objects name directly
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+const myNewArray = [200, 400, 500, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
+
