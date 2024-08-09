2ca5192e8f3ab4b25193127515c472a8bab63160
Piyush220803 <158078484+Piyush220803@users.noreply.github.com>
2024-08-09 14:19:21 +0530

    Add files via upload

---
+++ 01_basics/05_iterations/two.js
@@ -0,0 +1,22 @@
+// while loop
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Index value is: ${index}`);
+//     // index += 2
+//     index = index +2
+// }
+
+// let myArray = ['flash','batman','superman'] 
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+let score = 1
+do {
+    console.log(`The score is ${score}`);
+    score++
+} while ( score <= 10);
